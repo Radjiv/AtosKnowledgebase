9fb4c5f3bdce61e28ed7e6b85893f35c7ccf1ed1
kevinr1987 <kevin.rovers@atos.net>
2015-02-03 14:27:56 +0100

    Wijzigingen in User en Database DAO en UserList

--- AtosKnowledgebase/src/dao/UserDaoImpl.java
+++ AtosKnowledgebase/src/dao/UserDaoImpl.java
@@ -7,30 +7,30 @@ import javax.inject.Inject;
 import database.DatabaseDao;
 
 public class UserDaoImpl implements UserDao {
-	
+
 	@Inject
 	private DatabaseDao database;
 
 	@Override
 	public void addUser(String id, String fn, String mn, String ln, String dob, String function, List<String> competence) {
-		database.saveUser(new User(id, fn, mn, ln, dob, function, competence));		
+		database.saveUser(new User(id, fn, mn, ln, dob, function, competence));
 	}
-	
+
 	@Override
-	public void deleteUser(String id){
+	public void deleteUser(String id) {
 		database.deleteUser(id);
 	}
 
 	@Override
-	public void updateUser(String id, String fn, String mn, String ln, String dob, String function, List<String> competence) {;
+	public void updateUser(String id, String fn, String mn, String ln, String dob, String function, List<String> competence) {
 		database.updateUser(new User(id, fn, mn, ln, dob, function, competence), id);
 	}
-	
+
 	@Override
 	public List<User> getUsers() {
 		return database.getUsers();
 	}
-	
+
 	@Override
 	public User getUser(String id) {
 		return database.getUser(id);
--- AtosKnowledgebase/src/database/DatabaseDaoImpl.java
+++ AtosKnowledgebase/src/database/DatabaseDaoImpl.java
@@ -7,18 +7,19 @@ import java.util.List;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 import org.mongodb.morphia.query.Query;
+
 import com.mongodb.MongoClient;
 
 import dao.User;
 
-public class DatabaseDaoImpl implements DatabaseDao{
-		
-	private Datastore datastore;	
-	private String databaseName = "atosdb";
-		
-	public DatabaseDaoImpl(){
+public class DatabaseDaoImpl implements DatabaseDao {
+
+	private Datastore datastore;
+	private final String databaseName = "atosdb";
+
+	public DatabaseDaoImpl() {
 		try {
-			MongoClient mongoClient = new MongoClient( "localhost" , 27017 );			
+			MongoClient mongoClient = new MongoClient("localhost", 27017);
 			Morphia morphia = new Morphia();
 			morphia.mapPackage("database");
 			datastore = morphia.createDatastore(mongoClient, databaseName);
@@ -29,19 +30,19 @@ public class DatabaseDaoImpl implements DatabaseDao{
 
 	@Override
 	public void saveUser(User user) {
-		datastore.save(user);	
+		datastore.save(user);
 	}
 
 	@Override
 	public List<User> getUsers() {
 		List<User> users = new ArrayList<>();
-		Query query = datastore.createQuery(User.class);//creates a query specific for that dbcollection
-		//List list = query.asList();//gets all entities from that query
-		for (int i = 0 ; i < query.asList().size(); i ++){
-			User user = (User)query.asList().get(i);
+		Query<User> query = datastore.createQuery(User.class);// creates a query specific for that dbcollection
+		// List list = query.asList();//gets all entities from that query
+		for (int i = 0; i < query.asList().size(); i++) {
+			User user = query.asList().get(i);
 			users.add(user);
 		}
-		return users;	
+		return users;
 	}
 
 	@Override
@@ -52,12 +53,12 @@ public class DatabaseDaoImpl implements DatabaseDao{
 
 	@Override
 	public void updateUser(User user, String id) {
-		datastore.delete(id);
+		datastore.delete(User.class, id);
 		datastore.save(user);
 	}
 
 	@Override
 	public void deleteUser(String id) {
-		datastore.delete(id);
+		datastore.delete(User.class, id);
 	}
 }
