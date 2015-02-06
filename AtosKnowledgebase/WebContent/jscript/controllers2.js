ce6799cc67a5284bf99396a208194e2ac74d2b60
Radjiv <radjiv.raghoebier@atos.net>
2015-02-06 09:49:37 +0100

    andere jquery css en js ingeladen in de edit en add page de confirm dialog gegooid, maar de html check aan de voorkant doen t nie meer

--- AtosKnowledgebase/WebContent/jscript/controllers2.js
+++ AtosKnowledgebase/WebContent/jscript/controllers2.js
@@ -17,4 +17,42 @@ app.controller('AddPageCtrl', function($scope, $location, $http) {
 	for ( var selector in config) {
 		$(selector).chosen(config[selector]);
 	}
+	
+	function fnOpenNormalDialog() {
+		if (document.getElementById("id")!=""//moet regx pattern worden, voor check
+				|| document.getElementById("fn")!=""
+				|| document.getElementById("ln")!=""){
+			$("#dialog-confirm").html("Do you want to add a user?");
+
+			// Define the Dialog and its properties.
+			$("#dialog-confirm").dialog({
+				resizable: false,
+				modal: true,
+				title: "Add user",
+				height: 150,
+				width: 200,
+				buttons: {
+					"Yes": function () {
+						$(this).dialog('close');
+						callback(true);
+					},
+	                	"No": function () {
+	                		$(this).dialog('close');
+	                		callback(false);
+	                }
+				}
+			});
+		} else {
+			$("#dialog-confirm").html("Fill in required fields!");
+		}
+	}
+
+	$('#btnOpenDialog').click(fnOpenNormalDialog);
+
+	function callback(value) {
+	    if (value) {
+	    	$( "#addForm" ).trigger("submit");
+	    } 
+	}
+	
 });
--- AtosKnowledgebase/WebContent/jscript/controllers4.js
+++ AtosKnowledgebase/WebContent/jscript/controllers4.js
@@ -31,5 +31,37 @@ app.controller('EditPageCtrl', function getUser($scope, $http, userIdService) {
 		$('select[name="comp"]').find('option[value="'+ val +'"]').attr("selected",true);
 		$('.chosen-select').trigger('chosen:updated');
 	};
+	
+	function fnOpenNormalDialog() {
+	    $("#dialog-confirm").html("Do you want to edit user?");
+
+	    // Define the Dialog and its properties.
+	    $("#dialog-confirm").dialog({
+	        resizable: false,
+	        modal: true,
+	        title: "Edit user",
+	        height: 150,
+	        width: 200,
+	        buttons: {
+	            "Yes": function () {
+	                $(this).dialog('close');
+	                callback(true);
+	            },
+	                "No": function () {
+	                $(this).dialog('close');
+	                callback(false);
+	            }
+	        }
+	    });
+	}
+
+	$('#btnOpenDialog').click(fnOpenNormalDialog);
+
+	function callback(value) {
+	    if (value) {
+	    	$( "#editForm" ).trigger("submit");
+	    	document.location.href = '#/userList'
+	    } 
+	}
 
 });
