7b86634db1218603978349b039c2d8f3954d92f9
Radjiv <radjiv.raghoebier@atos.net>
2015-02-09 12:52:48 +0100

    wijzigingen van bas doorgevoerd

--- AtosKnowledgebase/WebContent/jscript/controllers3.js
+++ AtosKnowledgebase/WebContent/jscript/controllers3.js
@@ -1,43 +1,56 @@
-app.controller('ListPageCtrl', function getUsers($scope, $http, userIdService) {
+app.controller('ListPageCtrl', function getUsers($scope, $http, userIdService) {	
 	$http.get("rest/user/getUsers").success(function(response) {
 		$scope.users = response;
 	});
+	//deze commentaar kan worden verwijderd
 
 	$scope.onload = function() {
 		$scope.orderByField = 'id';
 		$scope.reverseSort = true;
-		$scope.searchBy = "ID";
-	};
-
-	$scope.addNewFilter = function(filterCount, searchVal){
-		var index = ["ID","Firstname","Middlename","Surname","Date of Birth","Function","Competence"];
-		var nr = index.indexOf(searchVal);
-		var tableElement = document.getElementById("advSearchBox");
-		var newTr = document.createElement("tr");
+		$scope.searchBy = "id";
+		$scope.filterId = true;
+		$scope.filterFn = true;
+		$scope.filterMn = true;
+		$scope.filterSn = true;
+		$scope.filterDoB = true;
+		$scope.filterF = true;
+		$scope.filterComp = true;
+		$scope.filterVip = true;
 		
-		if(filterCount <= 7){
-			index.splice(nr,1);
-			
-			//alert("De volgende filter krijgt nummer: "+ filterCount + " "+ searchVal);
-			
-			/*
-			  data-ng-attr-id = filterCount
-			  filterCount= parseInt(filterCount)+1;
-			  nieuwe tr met data-ng-attr-id + span + select tag met de values uit index + input 
-			 */
-			newTr.innerHTML = "blabalblalbalba";
-			tableElement.appendChild(newTr);
+	};
+	
+	$scope.addNewFilter = function(filter){
+		
+		switch (filter){
+		case 'id':
+			$scope.filterId = !$scope.filterId;
+			break;
+		case 'fn':
+			$scope.filterFn = !$scope.filterFn;
+			break;
+		case 'mn':
+			$scope.filterMn = !$scope.filterMn;
+			break;
+		case 'sn':
+			$scope.filterSn = !$scope.filterSn;
+			break;
+		case 'dob':
+			$scope.filterDoB = !$scope.filterDoB;
+			break;
+		case 'f':
+			$scope.filterF = !$scope.filterF;
+			break;
+		case 'comp':
+			$scope.filterComp = !$scope.filterComp;
+			break;
+		case 'vip':
+			$scope.filterVip = !$scope.filterVip;
+			break;
 		}
 	};
-
-	
 	
 	$scope.saveId = function(id) {
 		userIdService.addId(id);
-	};
-
-	$scope.deleteUser = function() {
-
 	};
 
 });
