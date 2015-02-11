e5ad78d0efe5f683411b96b10d84bd5fe2ca1996
a589768 <a589768@B701G12.ww930.my-it-solutions.net>
2015-02-11 10:11:18 +0100

    Wiki site

--- AtosKnowledgebase/WebContent/jscript/app.js
+++ AtosKnowledgebase/WebContent/jscript/app.js
@@ -9,6 +9,18 @@ var app = angular.module('myApp', [ 'ngRoute','myApp.service' ]);
 .when("/toDoList", {templateUrl: "partials/toDoList.html", controller: "ToDoListXMLCtrl"}) 
 .when("/editUser", {templateUrl: "partials/editUser.html", controller: "EditPageCtrl"})
 .when("/deleteUser", {templateUrl: "partials/deleteUser.html", controller: "DeletePageCtrl"})
+.when("/wiki", {templateUrl: "partials/wiki.html"})
 
 }]);
 
+var wikiApp = angular.module('wikiApp', ['ngRoute','myApp.service']);
+
+wikiApp.config(['$routeProvider', function ($routeProvider) { $routeProvider 
+.when("/wikihome", {templateUrl: "wikipages/WikiHome.html"})
+.when("/Database", {templateUrl: "wikipages/Database.html"})
+.when("/FrontEnd", {templateUrl: "wikipages/FrontEnd.html"})
+.when("/Restful", {templateUrl: "wikipages/Restful.html"})
+.when("/DAOService",{templateUrl: "wikipages/DAOService.html"})
+.when("/",{templateUrl: "wikipages/WikiHome.html"})
+	
+}]);
